66757b62a1f00a13c0d117f9a8b0e945ef322bf3
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-30 10:20:34 -0300

    feat : 미들웨어 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CommonModule } from './common/common.module';
+import { RequestMiddleware } from './common/utils/logger.middleware';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [CommonModule],
   providers: [PrismaService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RequestMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}

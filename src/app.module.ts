bf7c5989b4247a14de057bdf087f6a2349b36074
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-30 11:27:21 -0300

    feat : .env 환경변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,13 @@ import {
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { RequestMiddleware } from './common/utils/logger.middleware';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [CommonModule],
+  imports: [ConfigModule.forRoot(), CommonModule],
   providers: [PrismaService],
 })
 export class AppModule implements NestModule {

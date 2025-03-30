2701ce2fe416bd8bd34f57ebdea3fddb1cc9c92e
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-30 10:27:29 -0300

    feat : 전역 예외처리 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 import { winstonLogger } from './common/utils/winston.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = winstonLogger;
   app.useLogger(logger);
+  app.useGlobalFilters(new HttpExceptionFilter(logger));
   await app.listen(8000);
 }
 bootstrap();
